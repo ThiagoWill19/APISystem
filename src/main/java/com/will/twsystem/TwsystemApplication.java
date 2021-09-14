2745ad1e500ccc7a05393ffcaa7ce699e77c5949
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-09-14 18:13:52 -0300

    Project started

---
+++ src/main/java/com/will/twsystem/TwsystemApplication.java
@@ -0,0 +1,13 @@
+package com.will.twsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/twsystem/TwsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.twsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
