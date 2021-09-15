fb3d17581773e115fa5b22ecf41d330ae1f93cae
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-09-15 10:44:29 -0300

    Created ClientService class and package exceptions for services

---
+++ src/main/java/com/will/twsystem/services/ClientService.java
@@ -0,0 +1,22 @@
+package com.will.twsystem.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.will.twsystem.domain.Client;
+import com.will.twsystem.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	public Client find(int id) {
+		Optional<Client> obj =  clientRepository.findById(id);
+		return obj.orElseThrow(()-> new com.will.twsystem.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! ID:  " + id + ", "+ Client.class.getName()));
+	}
+}
